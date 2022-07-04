469d8780189c82ec430a5a120b0bf1d240298986
blnm <45343407+blnm@users.noreply.github.com>
2022-07-04 11:26:06 -0400

    Update prob1.cu

--- prob1.cu
+++ prob1.cu
@@ -58,14 +58,14 @@ int main()
 		cudaGetDeviceProperties(&devProp, dev);
 
 		// print relevant properties
-		printf("\tdevice name:\t\t\t\t%s\n",				devProp.name);
-		printf("\tclock rate:\t\t\t\t%d\n",					devProp.clockRate);
-		printf("\tnumber of SMs:\t\t\t\t%d\n",				devProp.multiProcessorCount);
-		printf("\tnumber of cores:\t\t\t%d\n",				getNumCores(devProp));
-		printf("\twarp size:\t\t\t\t%d\n",					devProp.warpSize);
+		printf("\tdevice name:\t\t\t\t%s\n",			devProp.name);
+		printf("\tclock rate:\t\t\t\t%d\n",			devProp.clockRate);
+		printf("\tnumber of SMs:\t\t\t\t%d\n",			devProp.multiProcessorCount);
+		printf("\tnumber of cores:\t\t\t%d\n",			getNumCores(devProp));
+		printf("\twarp size:\t\t\t\t%d\n",			devProp.warpSize);
 		printf("\tamount of global memory:\t\t%d\n",		devProp.totalGlobalMem);
 		printf("\tamount of const memory:\t\t\t%d\n",		devProp.totalConstMem);
-		printf("\tamount of shared memory per block:\t%d\n",devProp.sharedMemPerBlock);
+		printf("\tamount of shared memory per block:\t%d\n",	devProp.sharedMemPerBlock);
 		printf("\tnumber of registers per block:\t\t%d\n",	devProp.regsPerBlock);
 		printf("\tmaximum threads per block:\t\t%d\n",		devProp.maxThreadsPerBlock);
 		printf("\tmaximum size of dim per block:\t\t%d\n",	devProp.maxThreadsDim);
